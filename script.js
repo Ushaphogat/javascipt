d3c437733470653d44f1e8600de7adf27727191c
Ushaphogat <157911348+Ushaphogat@users.noreply.github.com>
2024-02-08 20:24:31 +0530

    2nd commit

--- script.js
+++ script.js
@@ -27,7 +27,20 @@ document.body.style.background = "cerulean";
 console.log("first child of b is: ", b.firstChild);
 console.log("first element child of b is: ", b.firstelementChild);*/
 
-let t = document.body.firstElementChild.firstElementChild;
+/*let t = document.body.firstElementChild.firstElementChild;
 console.log(t);
 console.log(t.rows);
 console.log(t.captions);
+*/
+
+// change the card title to red
+
+let ctitle = document.getElementsByClassName("card-title")[0];
+ctitle.style.color = "red";
+let ctitle = document.querySelectorAll(" .card-titles");
+ctitles[0].style.color = "blue";
+ctitles[1].style.color = "red";
+ctitles[2].style.color = "green";
+console.log("ctitles");
+document.querySelector(".this").style.color = "red";
+document.querySelector(".this").style.background = "cyan";
